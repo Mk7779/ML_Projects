27dfdb1a74ec16dcb20ade8f555452778cb766e7
Mk7779 <144084136+Mk7779@users.noreply.github.com>
2026-05-10 11:30:24 +0530

    Refactor Project-1.py to simplify navigation
    Removed unused data processing and plotting code.

--- new_file/Project-1.py
+++ new_file/Project-1.py
@@ -7,25 +7,9 @@ import numpy as np
 st.title('HOME')
 
 r = st.sidebar.radio('Navigation',['Home','Opration'])
-sb = st.selectbox('COLORS',["R",'Y','B','G'],index=2)
-
-data = {
-'num':[x for x in range(1,11)],
-'square':[x**2 for x in range(1,11)],
-'twice':[x*2 for x in range(1,11)],
-'thrice':[x*3 for x in range(1,11)],
-}
-if r=='Opration':
-    df = pd.DataFrame(data)
-    col = st.sidebar.selectbox('Select any number',df.columns)
+r = st.selectbox('COLORS',["R",'Y','B','G'],index=2)
 
 
-  
-    ax.set_title(f'Plot of {col} vs num')
-    ax.set_xlabel('num')
-    ax.set_ylabel(col)
-
-    st.pyplot(fig)
 if r =='Home':
     st.write('welcome')
     st.balloons()
