dca527cc776bd72f605455d8f169df8c3c41f3b1
Mk7779 <144084136+Mk7779@users.noreply.github.com>
2026-05-10 10:04:35 +0530

    Uncomment Streamlit code for interactive app

--- new_file/python.py
+++ new_file/python.py
@@ -3,42 +3,42 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# st.title("STREAMLIT SESSION")
+st.title("STREAMLIT SESSION")
 
-# st.header("Header")
+st.header("Header")
 
-# st.subheader("Sub header")
+st.subheader("Sub header")
 
-# st.text("The is an interactive streamlit application")
+st.text("The is an interactive streamlit application")
 
 
-# st.markdown("""
-# # h1 tag
-# ## h2 tag
-# ### h3 tag
+st.markdown("""
+# h1 tag
+## h2 tag
+### h3 tag
 
-# :moon: <br>
-# :sunglasses:
+:moon: <br>
+:sunglasses:
             
-# **bold**
-# _italic_
+**bold**
+_italic_
 
-# """
-# ,True)
+"""
+,True)
 
-# st.write(1,2,3,4)
+st.write(1,2,3,4)
 
-# st.write('The numbers are',[1,2,3,4,5])
+st.write('The numbers are',[1,2,3,4,5])
 
-# st.write("## The is a text")
+st.write("## The is a text")
 
-# dic = {
-#     "name":"guvi",
-#     'age':10,
-#     'place':'chennai'
-# }
+dic = {
+    "name":"guvi",
+    'age':10,
+    'place':'chennai'
+}
 
-# st.write(dic)
+st.write(dic)
 
 
 
