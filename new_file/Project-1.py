546f94ebbf3208682eac1138078e25a215311125
Mk7779 <144084136+Mk7779@users.noreply.github.com>
2026-05-13 18:52:25 +0530

    Remove Streamlit UI components from Project-1.py
    Removed various Streamlit UI elements from the app.

--- new_file/Project-1.py
+++ new_file/Project-1.py
@@ -4,14 +4,6 @@ import pandas as pd
 import numpy as np
 
 
-st.title('HOME')
-st.write('welcome')
-st.balloons()
-st.success('SUCCESSS!!!')
-st.error('Error')
-st.warning('warning')
-st.info('info')
-st.exception('exception')
 
 r = st.sidebar.radio('Navigation',['Home','Opration'])
 
